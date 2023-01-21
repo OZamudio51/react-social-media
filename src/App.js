1851108ad38370a16cf46a10e650e727c256f3c8
Oscar Zamudio <zamudio_1526@hotmail.com>
2023-01-21 11:18:23 -0700

    with '#' will be ignored, and an empty message aborts the commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback, createContext, useContext, useReducer } from "react";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import postReducer from "./reducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const UserContext = createContext();
+export const PostContext = createContext({
+    posts: []
+});
+
+const App = () => {
+    const [user, setUser] = useState("Oscar");
+    // const [posts, setPosts] = useState([]);
+    const initialPostState = useContext(PostContext);
+    const [state, dispatch] = useReducer(postReducer, initialPostState);
+
+    useEffect(() => {
+        document.title = user ? `${user}'s Feed`: "Please login";
+    }, [user]);
+
+    // const handleAddPost = useCallback((newPost) => {
+    //     setPosts([newPost, ...state.posts]);
+    // }, [state.posts]);
+
+    if (!user) {
+        return <Login setUser={setUser}/>;
+    }
+
+    return (
+        <PostContext.Provider value={{ state, dispatch }}>
+        <UserContext.Provider value={user}>
+            <Header user={user} setUser={setUser} />
+            <CreatePost user={user} 
+            // handleAddPost={handleAddPost}
+            />
+            <PostList posts={state.posts} />
+        </UserContext.Provider>
+        </PostContext.Provider>
+    );
+};
 
 export default App;
